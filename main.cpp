b207c0fa58641c97af1b08aea59c31d0c95233ae
mokkenz1 <lam1600@mail.ru>
2024-10-16 17:04:52 +0300

    Done main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include "matrix.hpp"
 
 int main()
 {
+  size_t M = 0, N = 0;
+  std::cin >> M >> N;
+  if (std::cin.fail())
+  {
+    return 1;
+  }
+  int ** table = nullptr;
+  try
+  {
+    table = createTable(M, N);
+  }
+  catch(const std::bad_alloc &e)
+  {
+    std::cerr << "Not enough memory\n";
+    return 2;
+  }
 }
